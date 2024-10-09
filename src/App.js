93583cf3e40d2fb9c80aa04806a1d0ca19d2a65d
Sumit Pant <sumitpant@Sumits-MacBook-Air.local>
2024-10-09 16:07:11 +0530

    main page

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
 import './App.css';
-import { Route,Routes } from 'react-router-dom';
-import InfiniteScroll from './component/InfiniteScroll';
+import {  useNavigate } from 'react-router-dom';
+
+import { Button, Tooltip } from '@mui/material';
 
 function App() {
+ const navigate= useNavigate();
   return (
     <div className="App">
-      <header className="App-header">    
+      <header className="App-header">
+        <div style={{ display: 'flex', flexWrap: 'wrap', gap: '0.5em' }}>
+          <Tooltip title="InfiniteScroll using intersection Observer">
+            <Button variant="contained" onClick={()=>{ navigate("/infinte-intersecton");}}>InfiniteScroll </Button>
+          </Tooltip>
+
+        </div>
+
       </header>
     </div>
   );
